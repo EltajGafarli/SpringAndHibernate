3bfb990fd471e92464aac20ff1b91051ef8847e1
Eltaj <83557585+EltajGafarli@users.noreply.github.com>
2023-02-01 23:29:24 +0400

    Update HibernateProperties.java

--- HibernateProperties.java
+++ HibernateProperties.java
@@ -9,9 +9,9 @@ public class HibernatePropertiesClass {
     static Properties getProperties() {
         Properties prop = new Properties();
         prop.setProperty("hibernate.connection.driver_class", "org.postgresql.Driver");
-        prop.setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5432/studentDB");
-        prop.setProperty("hibernate.connection.username", "postgres");
-        prop.setProperty("hibernate.connection.password", "190407011");
+        prop.setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5432/{enterDBName}");
+        prop.setProperty("hibernate.connection.username", "{username}");
+        prop.setProperty("hibernate.connection.password", "{password}");
         prop.setProperty("hibernate.connection.pool_size", "1");
         prop.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
         prop.setProperty("current_session_context_class", "thread");
